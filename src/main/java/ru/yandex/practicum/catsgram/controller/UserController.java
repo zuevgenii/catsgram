f140ef9267d0cadd11c6481126a5a9a18167c7fa
ZUEV EVGENII <zuevgenii@gmail.com>
2025-01-21 13:26:11 +0300

    add user controller and exceptions

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,2 @@
+package ru.yandex.practicum.catsgram.controller;public class UserController {
+}
---
+++ src/main/java/ru/yandex/practicum/catsgram/exception/InvalidEmailException.java
@@ -0,0 +1,2 @@
+package ru.yandex.practicum.catsgram.exception;public class InvalidEmailException {
+}
